9d4c75de089ff66b772843ca7cdc2d3a7ef716ca
Drasanart <mathieu.terrier13@gmail.com>
2017-05-21 14:41:46 +0200

    Add files via upload
    Les 3 avatars redimensionnée pour éviter les problèmes de taille
    Modif des 3 vue : Menu au cas ou
    VueParam terminé
    Vue commencer et à finir

--- Vue.java
+++ Vue.java
@@ -1,24 +1,139 @@
 package classe;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
 import javax.swing.JSeparator;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.Image;
 import java.awt.event.*;
 
 public class Vue extends JFrame implements ActionListener{
 	
-	public Vue()
+	public Vue(String nomTama, ImageIcon avatar)
 	{
-		constructVue();
+		int valeurInit = 100;
+		// Création du contenue //
+		Font f2 = new Font("Times", Font.PLAIN, 16);
+		
+		JPanel panel = new JPanel();
+        panel.setBackground(new Color(255,228,196));
+        
+        // layout manager //
+        panel.setLayout(new GridBagLayout());
+        
+        GridBagConstraints cel = new GridBagConstraints();
+        
+        // Label et progresse bar Top //
+        
+        JLabel lbInfoSante = new JLabel("Sante : ");
+        lbInfoSante.setFont(f2);
+        lbInfoSante.setPreferredSize(new Dimension(100,100));
+        cel.fill = GridBagConstraints.HORIZONTAL; 
+        cel.weightx = 0;
+        cel.gridx = 0;
+        cel.gridy = 0;
+        panel.add(lbInfoSante, cel);
+        
+        JLabel lbInfoFaim = new JLabel("Faim : ");
+        lbInfoFaim.setFont(f2);
+        lbInfoFaim.setPreferredSize(new Dimension(100,100));
+        cel.fill = GridBagConstraints.HORIZONTAL; 
+        cel.weightx = 0;
+        cel.gridx = 1;
+        cel.gridy = 0;
+        panel.add(lbInfoFaim, cel);
+        
+        JLabel lbInHygienne = new JLabel("hygienne : ");
+        lbInHygienne.setFont(f2);
+        lbInHygienne.setPreferredSize(new Dimension(100,100));
+        cel.fill = GridBagConstraints.HORIZONTAL; 
+        cel.weightx = 0;
+        cel.gridx = 2;
+        cel.gridy = 0;
+        panel.add(lbInHygienne, cel);
+        
+        JLabel lbInfoJoie = new JLabel("Joie : ");
+        lbInfoJoie.setFont(f2);
+        lbInfoJoie.setPreferredSize(new Dimension(100,100));
+        cel.fill = GridBagConstraints.HORIZONTAL; 
+        cel.weightx = 0;
+        cel.gridx = 3;
+        cel.gridy = 0;
+        cel.gridwidth = GridBagConstraints.REMAINDER;
+        panel.add(lbInfoJoie, cel);
+        
+        // Les progress bar //
+        
+        JProgressBar pbInfoSante = new JProgressBar();
+        pbInfoSante.setValue(valeurInit);
+        pbInfoSante.setPreferredSize(new Dimension(20,20));
+        cel.fill = GridBagConstraints.HORIZONTAL; 
+        cel.weightx = 1;
+        cel.gridx = 0;
+        cel.gridy = 1;
+        cel.gridwidth = 1;
+        panel.add(pbInfoSante, cel);
+        
+        JProgressBar pbInfoFaim = new JProgressBar();
+        pbInfoFaim.setValue(valeurInit);
+        pbInfoFaim.setPreferredSize(new Dimension(20,20));
+        cel.fill = GridBagConstraints.HORIZONTAL; 
+        cel.weightx = 1;
+        cel.gridx = 1;
+        cel.gridy = 1;
+        cel.gridwidth = 1;
+        panel.add(pbInfoFaim, cel);
+        
+        JProgressBar pbInfoHygienne = new JProgressBar();
+        pbInfoHygienne.setValue(valeurInit);
+        pbInfoHygienne.setPreferredSize(new Dimension(20,20));
+        cel.fill = GridBagConstraints.HORIZONTAL; 
+        cel.weightx = 1;
+        cel.gridx = 2;
+        cel.gridy = 1;
+        cel.gridwidth = 1;
+        panel.add(pbInfoHygienne, cel);
+        
+        JProgressBar pbInfoJoie = new JProgressBar();
+        pbInfoJoie.setValue(valeurInit);
+        pbInfoJoie.setPreferredSize(new Dimension(20,20));
+        cel.fill = GridBagConstraints.HORIZONTAL; 
+        cel.weightx = 1;
+        cel.gridx = 3;
+        cel.gridy = 1;
+        cel.gridwidth = 1;
+        cel.gridwidth = GridBagConstraints.REMAINDER;
+        panel.add(pbInfoJoie, cel);
+        
+        // Les boutons d'actions //
+        
+        JButton btSante = new JButton();
+        pbInfoJoie.setPreferredSize(new Dimension(20,20));
+        cel.weightx = 1;
+        cel.gridx = 0;
+        cel.gridy = 5;
+        cel.gridwidth = 1;
+        cel.gridheight = 1;
+        panel.add(btSante, cel);
+        
+		constructVue(panel);
 	}
 	
-	public void constructVue()
+	public void constructVue(JPanel p)
 	{
         setTitle("Tamago");
-    	
+        getContentPane().add(p);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // a modif si on veut arrette l'application d'ici
         setSize(800,600);
         setLocationRelativeTo(null);
@@ -32,8 +147,4 @@ public class Vue extends JFrame implements ActionListener{
     {
     }
 	
-    public static void main(String[] args)
-    {
-        Vue vueJeux = new Vue();
-    }
 }
--- VueParam.java
+++ VueParam.java
@@ -6,8 +6,10 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JSeparator;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
@@ -31,7 +33,9 @@ import javax.swing.JPanel;
 public class VueParam extends JFrame implements ActionListener {
 	
 	public JTextField jtfNomTama;
-	public Image img ;
+	public JRadioButton jfbAvatar1;
+	public JRadioButton jfbAvatar2;
+	public JRadioButton jfbAvatar3;
 	
 	public VueParam()
 	{
@@ -55,15 +59,17 @@ public class VueParam extends JFrame implements ActionListener {
         cel.weightx = 0;
         cel.gridx = 0;
         cel.gridy = 0;
+        cel.gridwidth = 1;
         panel.add(lbNomTama, cel);
         
         jtfNomTama = new JTextField();
         jtfNomTama.setPreferredSize(new Dimension(100,100));
         cel.fill = GridBagConstraints.HORIZONTAL;
         cel.ipady = 20;
-        cel.weightx = 1.5;
+        cel.weightx = 1;
         cel.gridx = 1;
         cel.gridy = 0;
+        cel.gridwidth = 1;
         cel.gridwidth = GridBagConstraints.REMAINDER; // fin de ligne
 		panel.add(jtfNomTama, cel);
 		
@@ -82,57 +88,97 @@ public class VueParam extends JFrame implements ActionListener {
         lbAvatar.setFont(f2);
         
         lbAvatar.setPreferredSize(new Dimension(100,100));
-        cel.fill = GridBagConstraints.HORIZONTAL; 
+        cel.fill = GridBagConstraints.HORIZONTAL;
+        cel.weightx = 0.5;
         cel.weightx = 0;
         cel.gridx = 0;
         cel.gridy = 2;
         cel.gridwidth = GridBagConstraints.REMAINDER; // fin de ligne
         panel.add(lbAvatar, cel);
         
-        // Les images //
-        JLabel lbAvatar1 = new JLabel();
+        // Les images // 
         
-        lbAvatar1.setFont(f2);
-        
-        lbAvatar1.setPreferredSize(new Dimension(100,100));
+        JLabel lbAvatar1 = new JLabel(new ImageIcon("./Ressources/avatar1.jpg"));
+        lbAvatar1.setSize(new Dimension(20,20));
         cel.fill = GridBagConstraints.HORIZONTAL; 
-        cel.weightx = 0;
+        cel.weightx = 0.5;
         cel.gridx = 0;
         cel.gridy = 3;
+        cel.gridheight = 1;
+        cel.gridwidth = 1;
         panel.add(lbAvatar1, cel);
         
-        ImageIcon avatar2 = new ImageIcon("./Ressources/avatar1.jpg");
-        Image im = avatar2.getImage();
-        im.getScaledInstance(10, 10, Image.SCALE_DEFAULT);
         
-        JLabel lbAvatar2= new JLabel();
-        lbAvatar2.setFont(f2);
-        
-        lbAvatar2.setPreferredSize(new Dimension(100,100));
-        //lbAvatar2.setIcon(avatar2);
+        JLabel lbAvatar2= new JLabel(new ImageIcon("./Ressources/avatar2.jpg"));;
+        lbAvatar2.setSize(new Dimension(20,20));
         cel.fill = GridBagConstraints.HORIZONTAL; 
-        cel.weightx = 0;
-        cel.gridx = 10;
+        cel.weightx = 0.5;
+        cel.gridx = 1;
         cel.gridy = 3;
+        cel.gridheight = 1;
+        cel.gridwidth = 1;
         panel.add(lbAvatar2, cel);
         
-        JLabel lbAvatar3 = new JLabel();
-        lbAvatar1.setFont(f2);
         
-        lbAvatar3.setPreferredSize(new Dimension(100,100));
-        cel.fill = GridBagConstraints.HORIZONTAL; 
-        cel.weightx = 0;
+        JLabel lbAvatar3 = new JLabel(new ImageIcon("./Ressources/avatar3.jpg"));
+        lbAvatar3.setSize(new Dimension(20,20));
+        cel.fill = GridBagConstraints.HORIZONTAL;
+        cel.weightx = 0.5;
         cel.gridx = 2;
         cel.gridy = 3;
+        cel.gridheight = 1;
+        cel.gridwidth = 1;
         cel.gridwidth = GridBagConstraints.REMAINDER; // fin de ligne
         panel.add(lbAvatar3, cel);
         
-        // JRadioButton
+        // JRadioButton //
+        jfbAvatar1 = new JRadioButton();
+        jfbAvatar1.setHorizontalAlignment(JRadioButton.CENTER);
+        jfbAvatar1.setBackground(new Color(255,228,196));
+        jfbAvatar1.setActionCommand("avatar1");
+        jfbAvatar1.addActionListener(this);
+        
+        cel.fill = GridBagConstraints.HORIZONTAL; 
+        cel.weightx = 0.5;
+        cel.gridx = 0;
+        cel.gridy = 4;
+        cel.gridheight = 1;
+        cel.gridwidth = 1;
+        panel.add(jfbAvatar1, cel);
+        
+        jfbAvatar2 = new JRadioButton();
+        jfbAvatar2.setHorizontalAlignment(JRadioButton.CENTER);
+        jfbAvatar2.setBackground(new Color(255,228,196));
+        jfbAvatar2.setActionCommand("avatar2");
+        jfbAvatar2.addActionListener(this);
+        
+        cel.fill = GridBagConstraints.HORIZONTAL; 
+        cel.weightx = 0.5;
+        cel.gridx = 1;
+        cel.gridy = 4;
+        cel.gridheight = 1;
+        cel.gridwidth = 1;
+        panel.add(jfbAvatar2, cel);
+        
+        jfbAvatar3 = new JRadioButton();
+        jfbAvatar3.setHorizontalAlignment(JRadioButton.CENTER);
+        jfbAvatar3.setBackground(new Color(255,228,196));
+        jfbAvatar3.setActionCommand("avatar3");
+        jfbAvatar3.addActionListener(this);
+        
+        cel.fill = GridBagConstraints.HORIZONTAL; 
+        cel.weightx = 0.5;
+        cel.gridx = 2;
+        cel.gridy = 4;
+        cel.gridheight = 1;
+        cel.gridwidth = 1;
+        cel.gridwidth = GridBagConstraints.REMAINDER; // fin de ligne
+        panel.add(jfbAvatar3, cel);
         
         // Insertion d'un saut de ligne //
         JLabel space2 = new JLabel("");
         space.setPreferredSize(new Dimension(200,600));
-        cel.gridy = 4;
+        cel.gridy = 5;
         cel.gridwidth = GridBagConstraints.REMAINDER; // fin de ligne
         panel.add(space2, cel);
         
@@ -143,9 +189,9 @@ public class VueParam extends JFrame implements ActionListener {
         Valider.addActionListener(this);
         
         cel.fill = GridBagConstraints.HORIZONTAL;
-        cel.weightx = 1.0;
-        cel.gridx = 1;
-        cel.gridy = 5;
+        cel.weightx = 0.5;
+        cel.gridx = 2;
+        cel.gridy = 6;
         cel.gridwidth = 1;
         cel.anchor = GridBagConstraints.LAST_LINE_END;
         panel.add(Valider, cel);
@@ -157,9 +203,9 @@ public class VueParam extends JFrame implements ActionListener {
         Annuler.addActionListener(this);
         
         cel.fill = GridBagConstraints.HORIZONTAL;
-        cel.weightx = 1.0;
+        cel.weightx = 0.5;
         cel.gridx = 0;
-        cel.gridy = 5;
+        cel.gridy = 6;
         cel.gridwidth = 1;
         cel.anchor = GridBagConstraints.LAST_LINE_START; //bottom of space
         panel.add(Annuler, cel);
@@ -171,22 +217,59 @@ public class VueParam extends JFrame implements ActionListener {
 	
     public void actionPerformed(ActionEvent e)
     {
+    	if(e.getActionCommand().equals("avatar1"))
+    	{
+    		jfbAvatar2.setSelected(false);
+    		jfbAvatar3.setSelected(false);
+    	}
+    	if(e.getActionCommand().equals("avatar2"))
+    	{
+    		jfbAvatar1.setSelected(false);
+    		jfbAvatar3.setSelected(false);
+    	}
+    	if(e.getActionCommand().equals("avatar3"))
+    	{
+    		jfbAvatar1.setSelected(false);
+    		jfbAvatar2.setSelected(false);
+    	}
         if(e.getActionCommand().equals("Valid"))
         {
-        	if(this.jtfNomTama.getText().isEmpty())
+        	if(this.jtfNomTama.getText().isEmpty() ||  ((this.jfbAvatar1.isSelected() == false) && (this.jfbAvatar2.isSelected() == false) && (this.jfbAvatar3.isSelected() == false)))
         	{
         		JFrame frame = new JFrame();
-        		JOptionPane.showMessageDialog(frame, "Veuillez renseigner un nom à votre Tamagotchi");
+        		JOptionPane.showMessageDialog(frame, "Veuillez renseigner un nom à votre Tamagotchi et sélectionner un avatar");
         	}
         	else
         	{
-            	Vue initVue = new Vue();
+        		Vue initVue;
+        		String nom = jtfNomTama.getText();
+        		ImageIcon img;
+        		
+        		if((this.jfbAvatar1.isSelected() == true))
+        		{
+        			img = new ImageIcon("./Ressources/avatar1.jpg");
+        			initVue = new Vue(nom, img);
+        		}
+        		if((this.jfbAvatar2.isSelected() == true))
+        		{
+        			img = new ImageIcon("./Ressources/avatar2.jpg");
+        			initVue = new Vue(nom, img);
+        		}
+        		if((this.jfbAvatar3.isSelected() == true))
+        		{
+        			img = new ImageIcon("./Ressources/avatar3.jpg");
+        			initVue = new Vue(nom, img);
+        		}
+        		
             	this.dispose(); // fermeture de l'objet courant la fenetreM
         	}
         }
         if(e.getActionCommand().equals("Annul"))
         {
-        	;
+        	this.jtfNomTama.setText("");
+        	this.jfbAvatar1.setSelected(false);
+			this.jfbAvatar2.setSelected(false);
+			this.jfbAvatar3.setSelected(false);
         }
     }
 	
@@ -195,7 +278,7 @@ public class VueParam extends JFrame implements ActionListener {
         setTitle("Information Tamagotchi");
         getContentPane().add(p);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // a modif si on veut arrette l'application d'ici
-        setSize(400,220);
+        setSize(800,360); // 400 220
         setLocationRelativeTo(null);
         setAlwaysOnTop(false);
         setResizable(false);
@@ -203,8 +286,4 @@ public class VueParam extends JFrame implements ActionListener {
         setVisible(true);
 	}
 	
-    public static void main(String[] args)
-    {
-        Vue vueJeux = new Vue();
-    }
 }
